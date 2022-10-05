2c71f203737fe1dd10af70692647b9fc57193b15
Masan-k <uronti@hotmail.co.jp>
2022-10-06 01:17:19 +0900

    create increment

--- socket/increment/incrementServer.py
+++ socket/increment/incrementServer.py
@@ -4,6 +4,8 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+count = 0
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
@@ -12,8 +14,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
       while True:
         data = conn.recv(1024)
-        #if not data: break
-        conn.sendall(data)
+        count = count + 1
+        sendMessage = "(" + str(count) + ")" + data.decode()
+        conn.sendall(sendMessage.encode())
         if data.decode() == "end":
           print("input end ...exit!")
           break 

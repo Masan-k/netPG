f21579393411f70d937fde6370aae5e69253af2d
Masan-k <uronti@hotmail.co.jp>
2022-10-10 00:15:22 +0900

    CREATE interactive

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 通信対戦ゲームを作成するために書いたPython + socketのコードを掲載
 
 # CODE
-|SOURCE|ABOUT|REFERENCES|
+|SOURCE|ABOUT|REFERENCES|DATE UPDATE|
 ----|----|----
-|[echo-origin](socket/echo-origin)|Hello world|[docs.python](https://docs.python.org/ja/3/library/socket.html#example)|
-|[input-loop](socket/input-loop)|input and loop|[echo-origin](socket/input-loop)<br>[Qiita-Socket()とかBind()とかを理解する](https://qiita.com/Michinosuke/items/0778a5344bdf81488114)|
-|[increment](socket/increment)|server integer increment|[input-loop](socket/input-loop)|
-|[first-UDP](socket/firstUdp)|Hello world(UDP)|[Pythonによるソケットプログラミング(UDP編) ](https://note-tech.com/python_socket_programming_udp/?preview=true)|
-|interactive|Two-way messaging|[first-UDP](socket/firstUdp)|
+|[echo-origin](socket/echo-origin)|Hello world|[docs.python](https://docs.python.org/ja/3/library/socket.html#example)|2022-10-10|
+|[input-loop](socket/input-loop)|input and loop|[GitHub-echo-origin](socket/input-loop)<br>[Qiita-Socket()とかBind()とかを理解する](https://qiita.com/Michinosuke/items/0778a5344bdf81488114)|2022-10-10|
+|[increment](socket/increment)|server integer increment|[GitHub-input-loop](socket/input-loop)|2022-10-10|
+|[first-UDP](socket/firstUdp)|Hello world(UDP)|[Pythonによるソケットプログラミング(UDP編) ](https://note-tech.com/python_socket_programming_udp/?preview=true)|2022-10-10|
+|[interactive](socket/interactive)|Two-way messaging|[GitHub-first-UDP](socket/firstUdp)|2022-10-10|
---
+++ socket/interactive/server2.py
@@ -0,0 +1,59 @@
+import socket
+import time
+import threading
+import pygame
+import pygame.locals
+import sys
+import queue 
+
+M_SIZE = 1024
+
+HOST = '127.0.0.1'
+PORT1 = 8890
+PORT2 = 8891
+FROM_ADDRESS = (HOST, PORT2)
+TO_ADDRESS = (HOST,PORT1)
+
+sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
+sock.bind(FROM_ADDRESS)
+
+q = queue.Queue()
+receiveMessage = "none"
+def receive():
+  while True:
+    receiveMessage, cli_addr = sock.recvfrom(M_SIZE)
+    receiveMessage = receiveMessage.decode(encoding='utf-8')
+    q.put(receiveMessage)
+
+def sendMessage(inputMessage):
+  send_len = sock.sendto(inputMessage.encode('utf-8'), TO_ADDRESS)
+  rx_message, addr = sock.recvfrom(M_SIZE)
+
+pygame.init()
+mainInfoFont = pygame.font.SysFont("ubuntu",15)
+screen = pygame.display.set_mode([300,300])
+pygame.display.set_caption("interactive")
+
+t_receive = threading.Thread(target=receive)
+t_receive.start()
+
+inputKey = ""
+
+while True:
+  pygame.display.update()
+  screen.fill((0,0,0))
+
+  while not q.empty():
+    receiveMessage = q.get()
+
+  for event in pygame.event.get():
+    if event.type == pygame.locals.KEYDOWN: 
+      inputKey = pygame.key.name(event.key) 
+      t_sendMessage = threading.Thread(target=sendMessage, args=(inputKey,))
+      t_sendMessage.start() 
+      if event.key == pygame.locals.K_ESCAPE:
+        pygame.quit()
+        sys.exit()
+  screen.blit(mainInfoFont.render('server2 inputKey >> ' + inputKey, True, (255, 255, 255)), [10,10]) 
+  screen.blit(mainInfoFont.render('received message  >> ' + receiveMessage, True, (255, 255, 255)), [10,30]) 
+

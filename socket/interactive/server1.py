f3bec6bb1d81e4ac56f01e4e970d1d76d9a839ac
Masan-k <uronti@hotmail.co.jp>
2022-10-10 00:35:53 +0900

    up

--- socket/interactive/server1.py
+++ socket/interactive/server1.py
@@ -26,7 +26,6 @@ def receive():
 
 def sendMessage(inputMessage):
   send_len = sock.sendto(inputMessage.encode('utf-8'), TO_ADDRESS)
-  rx_message, addr = sock.recvfrom(M_SIZE)
 
 pygame.init()
 mainInfoFont = pygame.font.SysFont("ubuntu",15)
@@ -34,6 +33,7 @@ screen = pygame.display.set_mode([300,300])
 pygame.display.set_caption("interactive")
 
 t_receive = threading.Thread(target=receive)
+t_receive.setDaemon(True)
 t_receive.start()
 
 inputKey = ""
@@ -47,12 +47,14 @@ while True:
 
   for event in pygame.event.get():
     if event.type == pygame.locals.KEYDOWN: 
-      inputKey = pygame.key.name(event.key) 
-      t_sendMessage = threading.Thread(target=sendMessage, args=(inputKey,))
-      t_sendMessage.start() 
       if event.key == pygame.locals.K_ESCAPE:
         pygame.quit()
         sys.exit()
+      else:
+        inputKey = pygame.key.name(event.key) 
+        t_sendMessage = threading.Thread(target=sendMessage, args=(inputKey,))
+        t_sendMessage.start() 
+
   screen.blit(mainInfoFont.render('server1 inputKey >> ' + inputKey, True, (255, 255, 255)), [10,10]) 
   screen.blit(mainInfoFont.render('received message  >> ' + receiveMessage, True, (255, 255, 255)), [10,30]) 
 
--- socket/interactive/server2.py
+++ socket/interactive/server2.py
@@ -27,7 +27,7 @@ def receive():
 
 def sendMessage(inputMessage):
   send_len = sock.sendto(inputMessage.encode('utf-8'), TO_ADDRESS)
-  rx_message, addr = sock.recvfrom(M_SIZE)
+  #rx_message, addr = sock.recvfrom(M_SIZE)
 
 pygame.init()
 mainInfoFont = pygame.font.SysFont("ubuntu",15)
@@ -35,6 +35,7 @@ screen = pygame.display.set_mode([300,300])
 pygame.display.set_caption("interactive")
 
 t_receive = threading.Thread(target=receive)
+t_receive.setDaemon(True)
 t_receive.start()
 
 inputKey = ""
@@ -48,12 +49,14 @@ while True:
 
   for event in pygame.event.get():
     if event.type == pygame.locals.KEYDOWN: 
-      inputKey = pygame.key.name(event.key) 
-      t_sendMessage = threading.Thread(target=sendMessage, args=(inputKey,))
-      t_sendMessage.start() 
       if event.key == pygame.locals.K_ESCAPE:
         pygame.quit()
         sys.exit()
+      else:
+        inputKey = pygame.key.name(event.key) 
+        t_sendMessage = threading.Thread(target=sendMessage, args=(inputKey,))
+        t_sendMessage.start() 
+
   screen.blit(mainInfoFont.render('server2 inputKey >> ' + inputKey, True, (255, 255, 255)), [10,10]) 
   screen.blit(mainInfoFont.render('received message  >> ' + receiveMessage, True, (255, 255, 255)), [10,30]) 
 
